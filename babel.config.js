983486afa1926ad2f45a4b035b60edddcb6c7832
Nityan Khanna <nityan.khanna@gmail.com>
2023-08-29 19:27:36 -0400

    updates

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  presets: ['@vue/cli-plugin-babel/preset'],
-  plugins: ['@babel/plugin-proposal-private-methods', '@babel/plugin-proposal-class-properties']
+  presets: [
+    '@vue/cli-plugin-babel/preset'
+  ]
 }
